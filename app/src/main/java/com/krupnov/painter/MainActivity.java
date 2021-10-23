392868c2faa58e214f4d80357666ad79e44910ca
Крупнов Михаил <krupnovmn@gmail.com>
2021-10-24 00:33:16 +0300

    Add ability to draw

--- app/src/main/java/com/krupnov/painter/MainActivity.java
+++ app/src/main/java/com/krupnov/painter/MainActivity.java
@@ -3,12 +3,35 @@ package com.krupnov.painter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    private DrawingView drawingView;
+    private ImageButton currPaint;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        drawingView = (DrawingView) findViewById(R.id.drawing);
+        LinearLayout paintLayout = (LinearLayout) findViewById(R.id.paint_colors);
+        currPaint = (ImageButton) paintLayout.getChildAt(0);
+        currPaint.setImageDrawable(getResources().getDrawable(R.drawable.paint_pressed));
+
     }
+
+    public void paintClicked(View view) {
+        if (view != currPaint) {
+            ImageButton imgView = (ImageButton) view;
+            String color = view.getTag().toString();
+            drawingView.setColor(color);
+            imgView.setImageDrawable(getResources().getDrawable(R.drawable.paint_pressed));
+            currPaint.setImageDrawable(getResources().getDrawable(R.drawable.paint));
+            currPaint = (ImageButton) view;
+        }
+    }
+
 }
